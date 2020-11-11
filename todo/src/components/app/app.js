7062114ccd56e4baa80fbc541e0f1274538ded61
Vova Kuzmenko <k.vovan.n@gmail.com>
2020-11-11 08:01:34 +0200

    add del element list

--- todo/src/components/app/app.js
+++ todo/src/components/app/app.js
@@ -1,20 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm from '../item-add-form';
 
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
 
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ];
+  maxId = 100;
 
+  state = {
+    todoData: [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 }
+    ]
+  };
+
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id);
+
+      const newArray = [
+        ...todoData.slice(0, idx),
+        ...todoData.slice(idx + 1)
+      ];
+
+      return {
+        todoData: newArray
+      };
+    });
+  };
+
+  addItem = (text) => {
+    
+    const newItem = {
+      label: text,
+      important: false,
+      id: this.maxId++
+    };
+
+  this.setState(({ todoData }) => {
+    const newArr = [
+      ...todoData,
+      newItem
+    ];
+
+    return {
+      todoData: newArr
+    };
+  });
+
+};
+
+render() {
   return (
     <div className="todo-app">
       <AppHeader toDo={1} done={3} />
@@ -23,9 +65,13 @@ const App = () => {
         <ItemStatusFilter />
       </div>
 
-      <TodoList todos={todoData} />
+      <TodoList 
+        todos={this.state.todoData}
+        onDeleted={ this.deleteItem } />
+
+        <ItemAddForm onItemAdded={this.addItem}/>  
     </div>
   );
 };
+}
 
-export default App;
